a497a8a7539f72ed13f00dcd5482377b6cf558e9
maritos <nyarun.otaki@gmail.com>
2025-06-17 00:31:52 +0900

    typo修正

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <>
       <div>
-      <p>maritosのページにようこそ<p>
+      <p>maritosのページにようこそ</p>
       </div>
       <div>
         <a href="https://vite.dev" target="_blank">
