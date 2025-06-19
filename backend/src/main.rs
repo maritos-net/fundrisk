411f4cfc25dfa17fb3e707c36bfa1c114b9c85e9
maritos <nyarun.otaki@gmail.com>
2025-06-19 23:30:52 +0900

    URLのフォーマットを作成

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,10 +1,28 @@
 use reqwest::Client;
 use serde_json::Value;
+use proconio::input;
 
 // asyncは非同期処理を行うことを示す。.awaitをつけることで実行するし、非同期処理の完了を待つ。
 // awaitをつけるのは処理を記述した段階では実行（メモリ確保含む）を行わず、処理自体を指すawaitがあってから実行されることを示す。
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+
+    // ユーザーからの入力
+    println!("入力してください（例: AAPL 2mo 1d）:");
+    input! {
+        symbol: String,
+        range: String,
+        interval: String,
+    }
+
+    //　URLのフォーマット
+    let url = format!(
+        "https://query1.finance.yahoo.com/v8/finance/chart/{}?range={}&interval={}",
+        symbol, range, interval
+    );
+
+    println!("[debug] URL = {}", url);
+
     let url = "https://query1.finance.yahoo.com/v8/finance/chart/AAPL?range=2mo&interval=1d";
 
     // 現時点の理解としてはClientは架空のブラウザのようなもので、後述するユーザーエージェントのような性質を定義してHTTPリクエストを送信するためのもの
